83a7552b17a1abd6570310478d1cbf67219efbbc
Jérémie Dentan <jeremie.dentan@live.com>
2023-01-16 15:32:19 +0100

    V0 protein protT5 embeddings

---
+++ src/protBert/protbert.py
@@ -0,0 +1,91 @@
+import typing as t
+from transformers import T5EncoderModel, T5Tokenizer
+import torch
+import time
+
+from tqdm import tqdm
+
+from src.utils.pathtools import project
+from src.utils.sequence_data import sequence_data
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+checkpoint = project.data / 'protT5_checkpoints' / 'secstruct_checkpoint.pt'
+output = project.output / 'prot_embeddings.h5'
+sequences = {
+    k: item
+    for k, item in enumerate(sequence_data.sequences_validation[:10])
+}
+
+def get_T5_model():
+    model = T5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_half_uniref50-enc")
+    model = model.to(device) # move model to GPU
+    model = model.eval() # set model to evaluation model
+    tokenizer = T5Tokenizer.from_pretrained('Rostlab/prot_t5_xl_half_uniref50-enc', do_lower_case=False)
+
+    return model, tokenizer
+
+
+
+def get_embeddings( model, tokenizer, seqs: t.Dict[int, str], 
+                   max_residues=4000, max_seq_len=1000, max_batch=100 ):
+
+    results = {"residue_embs" : dict(), 
+               "protein_embs" : dict(),
+               "sec_structs" : dict() 
+               }
+
+    # sort sequences according to length (reduces unnecessary padding --> speeds up embedding)
+    seq_dict   = sorted( seqs.items(), key=lambda kv: len( seqs[kv[0]] ), reverse=True )
+    start = time.time()
+    batch = list()
+    for seq_idx, (pdb_id, seq) in enumerate(seq_dict, 1):
+        seq = seq
+        seq_len = len(seq)
+        seq = ' '.join(list(seq))
+        batch.append((pdb_id,seq,seq_len))
+
+        # count residues in current batch and add the last sequence length to
+        # avoid that batches with (n_res_batch > max_residues) get processed 
+        n_res_batch = sum([ s_len for  _, _, s_len in batch ]) + seq_len 
+        if len(batch) >= max_batch or n_res_batch>=max_residues or seq_idx==len(seq_dict) or seq_len>max_seq_len:
+            pdb_ids, seqs, seq_lens = zip(*batch)
+            batch = list()
+
+            # add_special_tokens adds extra token at the end of each sequence
+            token_encoding = tokenizer.batch_encode_plus(seqs, add_special_tokens=True, padding="longest")
+            input_ids      = torch.tensor(token_encoding['input_ids']).to(device)
+            attention_mask = torch.tensor(token_encoding['attention_mask']).to(device)
+            
+            try:
+                with torch.no_grad():
+                    embedding_repr = model(input_ids, attention_mask=attention_mask)
+            except RuntimeError:
+                print("RuntimeError during embedding for {} (L={})".format(pdb_id, seq_len))
+                continue
+
+            for batch_idx, identifier in enumerate(pdb_ids): # for each protein in the current mini-batch
+                s_len = seq_lens[batch_idx]
+                # slice off padding --> batch-size x seq_len x embedding_dim  
+                emb = embedding_repr.last_hidden_state[batch_idx,:s_len]
+                # apply average-pooling to derive per-protein embeddings (1024-d)
+                protein_emb = emb.mean(dim=0)
+                results["protein_embs"][identifier] = protein_emb.detach().cpu().numpy().squeeze()
+
+
+    passed_time=time.time()-start
+    avg_time = passed_time/len(results["protein_embs"])
+    print('\n############# EMBEDDING STATS #############')
+    print('Total number of per-residue embeddings: {}'.format(len(results["residue_embs"])))
+    print('Total number of per-protein embeddings: {}'.format(len(results["protein_embs"])))
+    print("Time for generating embeddings: {:.1f}[m] ({:.3f}[s/protein])".format(
+        passed_time/60, avg_time ))
+    print('\n############# END #############')
+    return results
+
+# Load the encoder part of ProtT5-XL-U50 in half-precision (recommended)
+model, tokenizer = get_T5_model()
+
+# Compute embeddings and/or secondary structure predictions
+results = get_embeddings(model, tokenizer, sequences)
+print(results)
