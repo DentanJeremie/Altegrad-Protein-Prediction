f1d53b83dab1dd99fc502884fee07f6e1d5b708f
Jérémie Dentan <jeremie.dentan@live.com>
2023-01-17 14:00:12 +0100

    TF IDF vectorization

--- src/embeddings/sequence_baseline_edited.py
+++ src/embeddings/sequence_baseline_edited.py
@@ -1,88 +1,46 @@
-import csv
-import numpy as np
-import scipy.sparse as sp
+import pandas as pd
+from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
 
 from src.utils.constants import SEQUENCE_BASELINE_FEATURE_NAME as FEATURE_NAME
 from src.utils.pathtools import project
 from src.utils.logging import logger
 from src.utils.sequence_data import SequenceData, sequence_data
+from src.utils.constants import *
 
 VECTORIZER_ANALYSER = 'char'
-VECTORIZER_NGRAMS_RANGE = (1, 3)
-LOGIT_SOLVER = 'liblinear'
+VECTORIZER_NGRAMS_RANGE = (1, 4)
+N_COMPONENT = 32
 
 
 class SequenceBaseline():
 
     def __init__(self, data: SequenceData = sequence_data):
         self.data: SequenceData = data
-        self._x_train: sp.csr_matrix = None
-        self._x_test: sp.csr_matrix = None
-        self._y_pred_proba: np.ndarray = None
-
-# ------------------ PROPERTIES ------------------
-
-    @property
-    def x_train(self):
-        if self._x_train is None:
-            self.do_vectorization()
-        return self._x_train
-
-    @property
-    def x_test(self):
-        if self._x_test is None:
-            self.do_vectorization()
-        return self._x_test
-
-    @property
-    def y_pred_proba(self):
-        if self._y_pred_proba is None:
-            self.do_logit_regression()
-        return self._y_pred_proba
 
 # ------------------ BUILDS ------------------
 
-    def do_vectorization(self):
+    def compute_embeddings(self):
         """Does the vectorization of the sequences, with TFIDF.
         Builds the attributes:
         * `self._x_train`
         * `self._x_test`
         """
+        logger.info('Computing TF-IDF vectorization...')
         vec = TfidfVectorizer(analyzer=VECTORIZER_ANALYSER, ngram_range=VECTORIZER_NGRAMS_RANGE)
-        self._x_train = vec.fit_transform(self.data.sequences_train)
-        self._x_test = vec.transform(self.data.sequences_test)
+        self.embeddings = vec.fit_transform(self.data.sequences)
+        
+        logger.info('Computing SVD of the vectors...')
+        svd = TruncatedSVD(n_components=N_COMPONENT)
+        self.embeddings = svd.fit_transform(self.embeddings)
 
-    def do_logit_regression(self):
-        """Does the logistic regression for the prediction.
-        Builds the attributes:
-        * `self._y_pred_proba`
-        """
-        clf = LogisticRegression(solver=LOGIT_SOLVER)
-        clf.fit(self.x_train, self.data.y_train)
-        self._y_pred_proba = clf.predict_proba(self.x_test)
+        output_path = project.get_new_embedding_file(TFIDF_EMBEDDING)
+        pd.DataFrame(self.embeddings).to_csv(output_path, index=False)
 
-# ------------------ PREDICTION ------------------
-
-    def predict(self):
-        """Does the prediction and saves it.
-        """
-        with project.get_new_feature_file(FEATURE_NAME).open('w') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',')
-            lst = list()
-            for i in range(18):
-                lst.append('class'+str(i))
-            lst.insert(0, "name")
-            writer.writerow(lst)
-            for i, protein in enumerate(self.data.proteins_test):
-                lst = self.y_pred_proba[i,:].tolist()
-                lst.insert(0, protein)
-                writer.writerow(lst)
 
 def main():
     sequence_baseline = SequenceBaseline()
-    sequence_baseline.predict()
+    sequence_baseline.compute_embeddings()
 
 if __name__ == '__main__':
     main()
