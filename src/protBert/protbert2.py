6ce821a31e0b3afaa95866bac709e5b84e81c837
Jérémie Dentan <jeremie.dentan@live.com>
2023-01-17 08:50:46 +0100

    protbert v3 with PCA reduction

--- src/protBert/protbert2.py
+++ src/protBert/protbert2.py
@@ -3,22 +3,26 @@ This scripts implements the computation of embeddings from the sequences.
 It uses a pretrained model, and is inspired from a code taken in https://github.com/agemagician/ProtTrans
 """
 
-import time
 import typing as t
 
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 import torch
-from transformers import T5EncoderModel, T5Tokenizer
+from transformers import AlbertTokenizer, XLNetTokenizer, AutoTokenizer, AutoModel, pipeline
 
 from src.utils.pathtools import project
 from src.utils.logging import logger
 from src.utils.sequence_data import SequenceData, sequence_data
 from src.utils.train_validation_test import SetsManager, sets_manager
 
-MODEL_NAME = 'Rostlab/prot_t5_xl_half_uniref50-enc'
-MAX_RESIDUES = 4000
-MAX_SEQ_LEN = 1000
-MAX_BATCH = 100
+TASK = 'feature-extraction'
+BERT_MODEL = 'Rostlab/prot_bert'
+ALBERT_MODEL = 'Rostlab/prot_albert'
+BERT_BFD_MODEL = 'Rostlab/prot_bert_bfd'
+XLNET_MODEL = 'Rostlab/prot_xlnet'
+N_COMPONENT_PCA = 32
 
 
 class ProtBertClassifier():
@@ -32,93 +36,123 @@ class ProtBertClassifier():
         self.data = data
         self.sets = sets
         
-        # Building model
+        # Building models...
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-        self.model = T5EncoderModel.from_pretrained(MODEL_NAME)
-        self.tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME, do_lower_case=False)
-        self.model = self.model.to(self.device) # move self.model to GPU
-        self.model = self.model.eval() # set self.model to evaluation model
+        # BERT
+        self.bert_model = AutoModel.from_pretrained(BERT_MODEL)
+        self.bert_tokenizer = AutoTokenizer.from_pretrained(BERT_MODEL, do_lower_case=False)
+        self.bert_pipeline = pipeline(TASK, model = self.bert_model, tokenizer=self.bert_tokenizer, device=self.device)
+        # ALBERT
+        self.albert_model = AutoModel.from_pretrained(ALBERT_MODEL)
+        self.albert_tokenizer = AlbertTokenizer.from_pretrained(ALBERT_MODEL, do_lower_case=False)
+        self.albert_pipeline = pipeline(TASK, model = self.albert_model, tokenizer=self.albert_tokenizer, device=self.device)
+        # BERT BFD
+        self.bert_bfd_model = AutoModel.from_pretrained(BERT_BFD_MODEL)
+        self.bert_bfd_tokenizer = AutoTokenizer.from_pretrained(BERT_BFD_MODEL, do_lower_case=False)
+        self.bert_bfd_pipeline = pipeline(TASK, model = self.bert_bfd_model, tokenizer=self.bert_bfd_tokenizer, device=self.device)
+        # XLNET
+        self.xlnet_model = AutoModel.from_pretrained(XLNET_MODEL)
+        self.xlnet_tokenizer = XLNetTokenizer.from_pretrained(XLNET_MODEL, do_lower_case=False)
+        self.xlnet_pipeline = pipeline(TASK, model = self.xlnet_model, tokenizer=self.xlnet_tokenizer, device=self.device)
 
         # Private attributes with properties
-        self._sequences_dict: t.Dict[int, str] = None
-        self._embeddings: t.Dict[str, np.ndarray] = None
-
-    @property
-    def sequences_dict(self):
-        if self._sequences_dict is None:
-            self.build_sequence_dict()
-        return self._sequences_dict
-
-    @property
-    def embeddings(self):
-        if self._embeddings is None:
-            self.compute_embeddings()
-        return self._embeddings
-
-    def build_sequence_dict(self):
-        """Builds the sequence dict.
-        It is a dict {protein_index -> protein_sequence}
-        """
-        self._sequences_dict = {index: item for index, item in enumerate(self.data.sequences[:10])}
+        self._bert_embeddings: t.Dict[str, np.ndarray] = None
+        self._albert_embeddings: t.Dict[str, np.ndarray] = None
+        self._bert_bfd_embeddings: t.Dict[str, np.ndarray] = None
+        self._xlnet_embeddings: t.Dict[str, np.ndarray] = None
     
     def compute_embeddings(self):
         """Computes the embeddings of the proteins.
         After execution, self.results is a dict{protein_index -> protein_embedding}
         The embeddings are np.ndarray of size 1024.
         """
-        logger.info(f'Computing embedding for {len(self.sequences)} protein sequences...')
-        # sort sequences according to length (reduces unnecessary padding --> speeds up embedding)
-        seq_dict   = sorted(
-            self.sequences.items(),
-            key=lambda kv: len(self.sequences[kv[0]] ),
-            reverse=True,
-        )
+        logger.info(f'Computing embedding for {len(self.data.sequences)} protein sequences...')
 
-        # Batch computation
-        self.embeddings: t.Dict[int, np.ndarray] = dict() 
-        start = time.time()
-        batch = list()
-        for seq_idx, (pdb_id, seq) in enumerate(seq_dict, 1):
-            seq = seq
-            seq_len = len(seq)
-            seq = ' '.join(list(seq))
-            batch.append((pdb_id,seq,seq_len))
+        sequences = self.data.sequences[:10]
 
-            # count residues in current batch and add the last sequence length to
-            # avoid that batches with (n_res_batch > max_residues) get processed 
-            n_res_batch = sum([s_len for  _, _, s_len in batch ]) + seq_len 
-            if len(batch) >= MAX_BATCH or n_res_batch>=MAX_RESIDUES or seq_idx==len(seq_dict) or seq_len>MAX_SEQ_LEN:
-                pdb_ids, seqs, seq_lens = zip(*batch)
-                batch = list()
+        logger.info(f'Computing BERT embeddings')
+        bert_embeddings_list = self.bert_pipeline(sequences)
+        self._bert_embeddings = [
+            np.mean(np.array(embedding[0][1:-1]), axis = 0)
+            for embedding in bert_embeddings_list
+        ]
 
-                # add_special_tokens adds extra token at the end of each sequence
-                token_encoding = self.tokenizer.batch_encode_plus(seqs, add_special_tokens=True, padding="longest")
-                input_ids      = torch.tensor(token_encoding['input_ids']).to(self.device)
-                attention_mask = torch.tensor(token_encoding['attention_mask']).to(self.device)
-                
-                try:
-                    with torch.no_grad():
-                        embedding_repr = self.model(input_ids, attention_mask=attention_mask)
-                except RuntimeError:
-                    logger.error("RuntimeError during embedding for {} (L={})".format(pdb_id, seq_len))
-                    continue
+        logger.info(f'Computing ALBERT embeddings')
+        albert_embeddings_list = self.albert_pipeline(sequences)
+        self._albert_embeddings = [
+            np.mean(np.array(embedding[0][1:-1]), axis = 0)
+            for embedding in albert_embeddings_list
+        ]
 
-                for batch_idx, identifier in enumerate(pdb_ids): # for each protein in the current mini-batch
-                    s_len = seq_lens[batch_idx]
-                    # slice off padding --> batch-size x seq_len x embedding_dim  
-                    emb = embedding_repr.last_hidden_state[batch_idx,:s_len]
-                    # apply average-pooling to derive per-protein embeddings (1024-d)
-                    protein_emb = emb.mean(dim=0)
+        logger.info(f'Computing BERT BFD embeddings')
+        bert_bfd_embeddings_list = self.bert_bfd_pipeline(sequences)
+        self._bert_bfd_embeddings = [
+            np.mean(np.array(embedding[0][1:-1]), axis = 0)
+            for embedding in bert_bfd_embeddings_list
+        ]
 
-                    # Saving the result
-                    self.embeddings[identifier] = protein_emb.detach().cpu().numpy().squeeze()
+        logger.info(f'Computing XLNET embeddings')
+        xlnet_embeddings_list = self.xlnet_pipeline(sequences)
+        self._xlnet_embeddings = [
+            np.mean(np.array(embedding[0][1:-1]), axis = 0)
+            for embedding in xlnet_embeddings_list
+        ]
 
-        # Timing
-        logger.info('Computation of the embedding of the proteins: done!')
-        passed_time=time.time()-start
-        avg_time = passed_time/len(self.embeddings)
-        logger.info('Total number of per-protein embeddings: {}'.format(len(self.embeddings)))
-        logger.info("Time for generating embeddings: {:.1f}[m] ({:.3f}[s/protein])".format(
-            passed_time/60, avg_time ))
+    def reduce_embedddings(self):
+        """Does a PCA reduction on the embeddings.
+        """
 
-protein = ProtBertClassifier()
+        logger.info('Doing scaling and PCA for BERT embeddings...')
+        # Scaling
+        embeddings_df = pd.DataFrame(self._bert_embeddings)
+        scaler = StandardScaler()
+        scaled_embeddings  = scaler.fit_transform(embeddings_df)
+        # PCA
+        pca = PCA(n_components=N_COMPONENT_PCA)
+        self._bert_embeddings = pca.fit_transform(scaled_embeddings)
+
+        logger.info('Doing scaling and PCA for ALBERT embeddings...')
+        # Scaling
+        embeddings_df = pd.DataFrame(self._albert_embeddings)
+        scaler = StandardScaler()
+        scaled_embeddings  = scaler.fit_transform(embeddings_df)
+        # PCA
+        pca = PCA(n_components=N_COMPONENT_PCA)
+        self._albert_embeddings = pca.fit_transform(scaled_embeddings)
+
+        logger.info('Doing scaling and PCA for BERT BFD embeddings...')
+        # Scaling
+        embeddings_df = pd.DataFrame(self._bert_bfd_embeddings)
+        scaler = StandardScaler()
+        scaled_embeddings  = scaler.fit_transform(embeddings_df)
+        # PCA
+        pca = PCA(n_components=N_COMPONENT_PCA)
+        self._bert_bfd_embeddings = pca.fit_transform(scaled_embeddings)
+
+        logger.info('Doing scaling and PCA for XLNET embeddings...')
+        # Scaling
+        embeddings_df = pd.DataFrame(self._xlnet_embeddings)
+        scaler = StandardScaler()
+        scaled_embeddings  = scaler.fit_transform(embeddings_df)
+        # PCA
+        pca = PCA(n_components=N_COMPONENT_PCA)
+        self._xlnet_embeddings = pca.fit_transform(scaled_embeddings)
+
+    def save_embeddings(self):
+        """Saves the embeddings to the disk.
+        """
+
+        path = project.output / 'embeddings' / 'protbert'
+        path.mkdir(parents=True, exist_ok=True)
+
+        pd.DataFrame(self._bert_embeddings).to_csv(path / 'bert_embeddings.csv')
+        pd.DataFrame(self._albert_embeddings).to_csv(path / 'albert_embeddings.csv')
+        pd.DataFrame(self._bert_bfd_embeddings).to_csv(path / 'bert_bfd_embeddings.csv')
+        pd.DataFrame(self._xlnet_embeddings).to_csv(path / 'xlnet_embeddings.csv')
+
+if __name__ == '__main__':
+    protbert = ProtBertClassifier()
+
+    protbert.compute_embeddings()
+    protbert.reduce_embedddings()
+    protbert.save_embeddings()
